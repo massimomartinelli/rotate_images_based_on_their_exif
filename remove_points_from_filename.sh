140be6de2d240d9d0fe63d754d91a20d6ce91c8c
Massimo <1863510+massimomartinelli@users.noreply.github.com>
2024-09-04 12:18:08 +0200

    Create remove_points_from_filename.sh

---
+++ remove_points_from_filename.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Find all files in the current directory and subdirectories
+find /home/user/MYDIR/ -type f | while read -r file; do   # CHANGE THE DIRECTORY
+  # Extract the extension and filename
+  extension="${file##*.}"
+  filename="${file%.*}"
+
+  # Remove all dots from the filename
+  clean_filename="${filename//./}"
+
+  # Construct the new filename
+  new_file="$clean_filename.$extension"
+
+  # Rename the file if the new filename is different
+  if [ "$file" != "$new_file" ]; then
+    mv "$file" "$new_file"
+  fi
+done
